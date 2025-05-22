6ae09ea72dd753b29b7f8159e60b9f6fed9bd2e9
Minzah Anees <minzahanees@gmail.com>
2025-05-22 03:39:46 -0500

    style: set up styling with tailwind and global css

--- apps/frontend/tailwind.config.mjs
+++ apps/frontend/tailwind.config.mjs
@@ -1,7 +1,8 @@
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
